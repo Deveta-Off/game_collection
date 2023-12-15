33f32d42eac236d453a92c0aa218af77c05339e6
Pastalavisto <96241542+Pastalavisto@users.noreply.github.com>
2023-12-15 10:58:04 +0100

    Database

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php 
-    require './controllers/homepage.php'
-?>
+    use Dotenv\Dotenv;
+    require __DIR__.'/vendor/autoload.php';
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    
+    require './controllers/homepage.php';
