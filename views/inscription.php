494654d21cfd0e52f9a34a78e209efddb8c3bf4a
Hugo Durand (i2203314) <i2203314@univ-lemans.fr>
2023-12-15 11:59:02 +0100

    update page inscription

---
+++ views/inscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/styles/inscription.css">
+    <title>Inscription</title>
+</head>
+<body>
+    <div class="container">
+        <form class="inscription">
+            <h2>Inscription</h2>
+
+            <label for="firstname">Prénom:</label>
+            <input type="text" id="firstname" name="firstname" required>
+
+            <label for="lastname">Nom:</label>
+            <input type="text" id="lastname" name="lastname" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Mot de passe:</label>
+            <input type="password" id="password" name="password" required>
+
+            <label for="confirm-password">Confirmer le mot de passe:</label>
+            <input type="password" id="confirm-password" name="confirm-password" required>
+
+            <button type="submit">S'inscrire</button>
+        </form>
+    </div>
+</body>
+</html>
