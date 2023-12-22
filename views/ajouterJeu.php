0a495b2ffdacba054a33e9a39f982f3d06351022
Theo Grenet (i2200213) <i2200213@univ-lemans.fr>
2023-12-22 14:44:32 +0100

    Initialisation page

---
+++ views/ajouterJeu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/styles/ajouterJeu.css">
+    <title>Ajouter un jeu</title>
+</head>
+
+<body>
+    <?php require './assets/components/header.php'; ?>
+    <h1>Ajouter un jeu à sa bibliothèque</h1>
+    <form>
+        <input type="text" />
+        <input type="submit" />
+    </form>
+    <?php require './assets/components/footer.php'; ?>
+</body>
+
+</html>
