b817f2129d1bc1b2be07c208f6d22c3a036783e8
Pastalavisto <96241542+Pastalavisto@users.noreply.github.com>
2023-12-22 16:17:20 +0100

    Leaderboard

--- assets/components/header.php
+++ assets/components/header.php
@@ -1,9 +1,9 @@
 <div class="header">
     <h1>Logo</h1>
     <div class="links">
-        <a href="/library">MA BIBLIOTHEQUE</a>
-        <a href="/addGame">AJOUTER UN JEU</a>
-        <a href="/leaderboard">CLASSEMENT</a>
+        <a href="./library">MA BIBLIOTHEQUE</a>
+        <a href="./addGame">AJOUTER UN JEU</a>
+        <a href="./leaderboard">CLASSEMENT</a>
         <a href="./connexion"><?php echo $isLoggedIn ? "PROFIL" : "CONNEXION" ?></a>
     </div>
 </div>
