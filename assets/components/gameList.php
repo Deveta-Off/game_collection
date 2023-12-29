91b97ed65a09d2598dc45f80de39334d43639224
Deveta-Off <theo.grenet.pro@gmail.com>
2023-12-29 23:50:50 +0100

    Généralisation affichage liste des jeux + Ajout condition d'affichage du bouton d'ajout + Ajout import bdd dans le model + Ajout animation au hover d'un jeu

--- assets/components/gameList.php
+++ assets/components/gameList.php
@@ -1,10 +1,29 @@
-<?php
-foreach($games as $game) {
+<div class="games">
+    <?php foreach ($games as $game) {
+        $platforms = getPlatforms($game["name_game"]);
+        $text_platforms = "";
+        foreach ($platforms as $platform) {
+            $text_platforms .= $platform["name_platform"] . ", ";
+        }
+        $text_platforms = substr($text_platforms, 0, -2);
     ?>
-        <div>
-            <h2>
-                <?php echo $game['name_game']; ?>
-            </h2>
+
+        <div class=game>
+            <div class="game-image">
+                <img src=<?= $game["url_image_game"] ?> alt="" />
+            </div>
+            <div class="game-infos">
+                <h1><?= $game["name_game"] ?></h1>
+                <div class="game-platforms">
+                    <p><?= $text_platforms ?></p>
+                </div>
+                <?php if ($page != "homepage") { ?>
+                    <form method="post">
+                        <input type="hidden" name="name_game" value="<?= $game["name_game"] ?>" />
+                        <input class="btn-game-add" name='submit' type="submit" value="Ajouter à la bibliothèque" />
+                    </form>
+                <?php } ?>
+            </div>
         </div>
-    <?php 
-    } ?>
+    <?php } ?>
+</div>
