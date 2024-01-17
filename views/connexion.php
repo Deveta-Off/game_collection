0e9f8c09f6539ab2e57fe0dd8b9215ba107e7bfc
Deveta-Off <theo.grenet.pro@gmail.com>
2024-01-17 17:17:53 +0100

    Fix css page connexion/inscription
    Le css devait être légèrement modifié + Ajout bouton qui redirige sur les pages inscription/connexion depuis leur page voisine

--- views/connexion.php
+++ views/connexion.php
@@ -4,19 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/styles/inscription.css">
+    <link rel="stylesheet" href="./assets/styles/auth.css">
     <title>Connexion</title>
 </head>
 
 <body>
     <div class="page-content">
-        <form class="inscription" method="post">
-            <h2>Connexion</h2>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-
-            <label for="password">Mot de passe:</label>
-            <input type="password" id="password" name="password" required>
+        <form class="auth" method="post">
+            <h2>Se connecter à Game Collection</h2>
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div>
+                <label for="password">Mot de passe:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <a href="./inscription">S'inscrire</a>
 
             <button type="submit">Se connecter</button>
         </form>
