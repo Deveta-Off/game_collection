c29f2fae5e509e9c103893953ab55cb6aa142598
Deveta-Off <theo.grenet.pro@gmail.com>
2024-01-17 23:19:45 +0100

    Ajout embed discord
    Quand on envoie l'url sur discord, on a un embed

--- views/homepage.php
+++ views/homepage.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Game Collection" property="og:title" />
+    <meta content="Un super site répertoriant tes jeux vidéos favoris !" property="og:description" />
     <title>Game Collection</title>
     <link rel="stylesheet" href="./assets/styles/homepage.css">
 </head>
