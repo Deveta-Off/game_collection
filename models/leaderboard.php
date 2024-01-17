c4088b502717c71fcc72872cf805260fdb14faeb
Pastalavisto <nathan.hameau17@gmail.com>
2024-01-17 23:44:09 +0100

    insertions sql

--- models/leaderboard.php
+++ models/leaderboard.php
@@ -6,7 +6,7 @@ $db = Database::getInstance()::getConnection();
 
 function getLeaderboard() {
     global $db;
-    $query = "SELECT name_user,surname_user,hours_played_game,name_game FROM LIBRARY INNER JOIN ACCOUNT ON ACCOUNT.Id_user = LIBRARY.Id_user ORDER BY hours_played_game DESC";
+    $query = "SELECT name_user,surname_user,hours_played_game,name_game FROM LIBRARY INNER JOIN ACCOUNT ON ACCOUNT.Id_user = LIBRARY.Id_user ORDER BY hours_played_game DESC LIMIT 20";
     $result = $db->query($query);
     $leaderboard = $result->fetchAll(PDO::FETCH_ASSOC);
     return $leaderboard;
